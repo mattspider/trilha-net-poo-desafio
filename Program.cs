6bdee60703d275b2cf0a27687f79961164b938d9
Matheus de Oliveira Soares <matt.oliveira.soares@gmail.com>
2025-07-26 02:54:53 -0300

    finalizado projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia:");
+        Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 32);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Facebook");
+    }
+}
